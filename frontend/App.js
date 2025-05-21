a5c8d28cc02064e5dd4d144764bd90bd35d11e32
Natalie Lim <natalielim1906@gmail.com>
2025-05-20 22:42:17 -0700

    fixed styling

--- frontend/App.js
+++ frontend/App.js
@@ -10,7 +10,7 @@ const Stack = createNativeStackNavigator()
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="StartingScreen">
+      <Stack.Navigator initialRouteName="StartingScreen" screenOptions={{headerShown: false}}>
         <Stack.Screen name="StartingScreen" component={StartingScreen} />
       </Stack.Navigator>
     </NavigationContainer>
