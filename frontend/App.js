964384b89cc8396f3359b5d3cf79d82adf3051c2
Natalie Lim <natalielim1906@gmail.com>
2025-05-20 22:37:28 -0700

    navigation works, styling got messed up

--- frontend/App.js
+++ frontend/App.js
@@ -1,38 +1,18 @@
-import { LinearGradient } from 'expo-linear-gradient';
-import { Image, Text, TouchableOpacity, View } from 'react-native';
-import moonImage from './assets/moon.png';
-import './global.css';
+
+import { NavigationContainer } from '@react-navigation/native';
+import 'react-native-gesture-handler';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import StartingScreen from './screens/StartingScreen';
+
+const Stack = createNativeStackNavigator()
 
 export default function App() {
   return (
-    <View className = 'flex-1'>
-      <LinearGradient colors={['#4e4e5d', '#21303f']}
-      style={{ flex: 1 }}
-      start={{ x: 0.5, y: 0 }}
-      end={{ x: 0.5, y: 1 }}>
-        <View className='items-center justify-center mt-28'>
-          <Image className='w-52 h-52' source={moonImage}/>
-        </View>
-      <Text className='text-center text-6xl font-medium text-white mt-32'> L U N A L O G</Text>
-      <Text className='text-center text-xl text-white mt-4 tracking-widest'> grow brighter like the moon </Text>
-      
-      {/* buttons */}
-      
-      <View className='items-center'>
-        <TouchableOpacity className='mt-20 bg-white h-16 w-96 justify-center rounded-3xl'>
-          <Text className='pb-16 text-center font-medium text-3xl text-custom-darker-blue mt-8 tracking-widest'>login</Text>
-        </TouchableOpacity>
-      </View>
-
-      <View className='items-center'>
-        <TouchableOpacity className='mt-8 bg-white h-16 w-96 justify-center rounded-3xl'>
-          <Text className='pb-16 text-center font-medium text-3xl text-custom-darker-blue mt-8 tracking-widest'>sign up</Text>
-        </TouchableOpacity>
-      </View>
-
-
-      </LinearGradient>
-    </View>
-    
-  );
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="StartingScreen">
+        <Stack.Screen name="StartingScreen" component={StartingScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
