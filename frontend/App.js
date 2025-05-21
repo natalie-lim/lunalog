6b018583a02d2647b41b58532ff4da15b46aa7b4
Natalie Lim <natalielim1906@gmail.com>
2025-05-20 23:43:34 -0700

    login outline

--- frontend/App.js
+++ frontend/App.js
@@ -1,17 +1,20 @@
 
 import { NavigationContainer } from '@react-navigation/native';
-import 'react-native-gesture-handler';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import 'react-native-gesture-handler';
 
 import StartingScreen from './screens/StartingScreen';
+import LoginScreen from 'screens/LoginScreen';
 
 const Stack = createNativeStackNavigator()
 
 export default function App() {
+  // defines the root of the app and wraps it in a navigationContainer, which enables navigation capabilities globally
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="StartingScreen" screenOptions={{headerShown: false}}>
         <Stack.Screen name="StartingScreen" component={StartingScreen} />
+        <Stack.Screen name="LoginScreen" component={LoginScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   )
