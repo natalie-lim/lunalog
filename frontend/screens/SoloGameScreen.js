99352caabaae7832d77dd86625dc59a96493d638
chrisk36 <chrisk36@seas.upenn.edu>
2025-06-09 13:07:56 +0800

    function for random math problems

--- frontend/screens/SoloGameScreen.js
+++ frontend/screens/SoloGameScreen.js
@@ -11,6 +11,52 @@ import useStore from '../store';
 export default function SoloGameScreen () {
     const timeDuration = useStore((state) => state.timeDuration);
     const [time, setTime] = useState(timeDuration);
+
+    //change to use zustand for following:
+    const asMin = 1;
+    const mdMin = 1;
+    const asMax = 100 + 1; //keep + 1
+    const mdMax = 20 + 1; //keep + 1
+    
+    //note: for sign, 1 = addition, 2 = subtraction, 3 = multiplication, 4 = division
+    //note: num1 should always just be displayed first (for division purposes)
+    function generateProblem() {
+        const sign = Math.floor(1 + (Math.random() * (4)));
+        let num1 = 0;
+        let num2 = 0;
+        let soln = 0;
+
+        console.log(sign);
+        if (sign == 1 || sign == 2) {
+            const x1 = Math.floor(asMin + (Math.random() * (asMax - asMin)));
+            const x2 = Math.floor(asMin + (Math.random() * (asMax - asMin)));
+            if (sign == 1) {
+                num1 = x1;
+                num2 = x2;
+                soln = num1 + num2;
+            } else {
+                num1 = Math.floor(Math.max(x1, x2));
+                num2 = Math.floor(Math.min(x1, x2));
+                soln = num1 - num2;
+            }
+        }
+
+        if (sign == 3 || sign == 4) {
+            const x1 = Math.floor(mdMin + (Math.random() * (mdMax - mdMin)));
+            const x2 = Math.floor(mdMin + (Math.random() * (mdMax - mdMin)));
+            if (sign == 1) {
+                num1 = x1;
+                num2 = x2;
+                soln = x1 * x2;
+            } else {
+                num1 = x1 * x2;
+                num2 = x1;
+                soln = x2;
+            }
+        }
+
+        return [sign, num1, num2, soln]
+    }
     
     useEffect(() => {
         const interval = setInterval(() => {
